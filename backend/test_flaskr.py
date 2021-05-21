a3c9507724cb0d962619b8122ba1eb54e8b29833
Kep <KepCreations@gmail.com>
2021-05-21 16:21:03 -0400

    add 404 error handler and /questions test

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format('postgres','password','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -41,6 +41,7 @@ class TriviaTestCase(unittest.TestCase):
       self.assertEqual(data['success'], True)
       self.assertTrue(data['total_questions'])
       self.assertTrue(len(data['questions']))
+      self.assertTrue(len(data['categories']))
 
     def test_404_sent_requesting_beyond_valid_page(self):
       res = self.client().get('/questions?page=1000')
@@ -48,7 +49,7 @@ class TriviaTestCase(unittest.TestCase):
 
       self.assertEqual(res.status_code, 404)
       self.assertEqual(data['success'], False)
-      # self.assertEqual(data['message'], 'resource not found')
+      self.assertEqual(data['message'], 'resource not found')
 
 
 
